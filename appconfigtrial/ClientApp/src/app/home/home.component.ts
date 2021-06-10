aff255c0f30b91980db21bcb2e1e4f26a401c2f1
Arif Ahmed <arifahmed2009@hotmail.co.uk>
2021-06-10 10:06:22 +0100

    Added some missing code. Also removed default code not required for Feature Toggle.

--- appconfigtrial/ClientApp/src/app/home/home.component.ts
+++ appconfigtrial/ClientApp/src/app/home/home.component.ts
@@ -6,20 +6,42 @@ import { Component, Inject } from "@angular/core";
   templateUrl: "./home.component.html",
 })
 export class HomeComponent {
-  featureToggles: FeatureTogglesResponse;
+  dataBasedOnFeatureOne: string;
+
+  featureOne: boolean;
+  featureTwo: boolean;
+
   constructor(http: HttpClient, @Inject("BASE_URL") baseUrl: string) {
     http
-      .get<FeatureTogglesResponse>(baseUrl + "weatherforecast/featuretoggles")
+      .get<boolean>(baseUrl + "feature/getFeature/FeatureOne")
       .subscribe(
         (result) => {
-          this.featureToggles = result;
+          this.featureOne = result;
+        },
+        (error) => console.error(error)
+    );
+    
+    http
+      .get<boolean>(baseUrl + "feature/getFeature/FeatureTwo")
+      .subscribe(
+        (result) => {
+          this.featureTwo = result;
+        },
+        (error) => console.error(error)
+    );
+    
+    http
+      .get<{ data: string }>(baseUrl + "feature/getFeatureOneDependantData")
+      .subscribe(
+        (result) => {
+          this.dataBasedOnFeatureOne = result.data;
         },
         (error) => console.error(error)
       );
   }
 }
 
-interface FeatureTogglesResponse {
-  funkySummary: boolean;
-  anotherFeature: boolean;
+interface Features {
+  featureOne: boolean;
+  featureTwo: boolean;
 }
---
+++ appconfigtrial/Controllers/FeatureController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.Mvc;
+using System.Threading.Tasks;
+
+namespace appconfigtrial.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FeatureController : ControllerBase
+    {
+        private IFeatureManagerSnapshot _featureManager;
+
+        public FeatureController(ILogger<FeatureController> logger, IConfiguration config, IFeatureManagerSnapshot featureManager)
+        {
+            _featureManager = featureManager;
+        }
+
+        [HttpGet]
+        [Route("GetFeatureOneDependantData")]
+        [FeatureGate(nameof(Features.FeatureOne))]
+        public ActionResult GetFeatureOneDependantData()
+        {
+            return Ok(new { Data = "Hello. If you see me then Feature One is active." });
+        }
+
+        [HttpGet]
+        [Route("GetFeature/{featureToggleName}")]
+        public async Task<ActionResult<bool>> GetFeatureToggle(string featureToggleName = "")
+        {
+            return Ok(await _featureManager.IsEnabledAsync(featureToggleName));
+        }
+    }
+
+    public class Features
+    {
+        public bool FeatureOne { get; set; }
+        public bool FeatureTwo { get; set; }
+    }
+}
